0d4ddd6645dbe2970aab365e1a419423ad27682c
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2019-11-04 16:41:57 +0900

    プロジェクト ファイルを追加します。

---
+++ GrobalHook.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrobalHook
+{
+    public static class MouseHook
+    {
+        public const int WH_MOUSE_LL = 14;
+
+        public delegate IntPtr HookProcedureDelegate(int _nCode, IntPtr _wParam, IntPtr _lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr SetWindowsHookEx(int _nHook, HookProcedureDelegate _lpfn, IntPtr _hInstance, int _nThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(IntPtr _hook);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern IntPtr CallNextHookEx(IntPtr _hook, int _nCode, IntPtr _wParam, IntPtr _lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(String _strModuleName);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class MouseHookStruct
+        {
+            public POINT pt;
+            public uint mouseData;
+            public uint flags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+    }
+}
